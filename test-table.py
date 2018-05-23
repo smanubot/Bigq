8d6fbca3f92647592622c6aa05a2a2d311344393
smanubot <39138344+smanubot@users.noreply.github.com>
2018-05-23 10:21:53 -0700

    Add files via upload
    This file is creates a simple table in Bigquery

---
+++ test-table.py
@@ -0,0 +1,27 @@
+import argparse
+import logging
+import pandas as pd
+import os
+from google.cloud import bigquery
+def create(bq_client,datasetid):
+    client = bigquery.Client()
+    dataset = datasetid
+    dataset_ref = client.dataset(dataset)
+    table_name = 'my_table'
+    table_ref = dataset_ref.table(table_name)
+    table = bigquery.Table(table_ref)
+    table = client.create_table(table)
+
+    assert table.table_id == 'my_table'
+def run(args):
+    bq_client = bigquery.Client(project=args.project)
+    create(bq_client,args.datasetid)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("project", help = "Project Identifier")
+    parser.add_argument("datasetid", help="Name of the dataset")
+   # parser.add_argument("input_directory", help = "Name of the Directory which contains the excel files")
+
+    logging.getLogger().setLevel(logging.INFO)
+    args = parser.parse_args()
+    run(args)
