1ccf264084b35a8d90ef0d828e78742e55ac9194
smanubot <39138344+smanubot@users.noreply.github.com>
2018-05-23 12:05:38 -0700

    Update num.py

--- num.py
+++ num.py
@@ -2,7 +2,6 @@
 a = 'jenkinsforclorox'
 c = b'jenkinsforclorox'
 # using encode() to encode the String
-# encoded version of a is stored in d
 # using ASCII mapping
 d = a.encode('ASCII')
 # checking if a is converted to bytes or not
